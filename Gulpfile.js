dbb729452591e267ad3e8601bb9838141f8551c3
Damien Erambert <damien.erambert@me.com>
2017-07-23 19:38:48 -0700

    Add a few gulp tasks to allow a dev server, building and static files copy

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ const handlebars = require('handlebars')
 const gulp = require('gulp')
 const hb = require('gulp-hb')
 const postcss = require('gulp-postcss')
+const browserSync = require('browser-sync').create()
 
 // Dirty hack to use partials with the .html extension 
 // See https://github.com/shannonmoeller/gulp-hb/issues/41#issuecomment-218832896
@@ -10,17 +11,53 @@ require.extensions['.html'] = (module, filename) => {
   module.exports = handlebars.compile(fs.readFileSync(filename, 'utf8'))
 }
 
+const destFolder = (path) => `./dist${path || ''}`
+
+const files = {
+  html: {
+    from: './src/*.html',
+    to: destFolder()
+  },
+  css: {
+    from: './src/css/index.css',
+    to: destFolder('/css')
+  },
+  static: {
+    from: [
+      './src/img/**/*'
+    ],
+    to: destFolder()
+  }
+}
+
 gulp.task('html', () => (
-  gulp.src('./src/*.html')
+  gulp.src(files.html.from)
   .pipe(hb({
     partials: './src/_includes/**/*.{hbs,html}'
   }))
-  .pipe(gulp.dest('./dist'))
+  .pipe(gulp.dest(files.html.to))
 ))
 
 gulp.task('css', () => (
-  gulp.src('./src/css/index.css')
-  .pipe(gulp.dest('./dist/css'))
+  gulp.src(files.css.from)
+  .pipe(gulp.dest(files.css.to))
+  .pipe(browserSync.stream())
 ))
 
-gulp.task('default', ['html', 'css'])
+gulp.task('static', () => (
+  gulp.src(files.static.from, { base: 'src' }).pipe(gulp.dest(files.static.to))
+))
+
+const beforeServe = ['html', 'css', 'static']
+
+gulp.task('default', beforeServe, () => {
+  browserSync.init({
+    server: destFolder()
+  })
+  
+  gulp.watch('./src/css/**/*.css', ['css'])
+  gulp.watch('./src/**/*.{html,hbs}', ['html'])
+  gulp.watch('./dist/**/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('build', beforeServe)
