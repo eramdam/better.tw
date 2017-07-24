fc86867a5d97824fa02cbc6ca3d75227ad9f82fe
Damien Erambert <damien.erambert@me.com>
2017-07-23 19:20:07 -0700

    Add a simple handlebars powered template system
    Uses gulp-hb

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,26 @@
+const fs = require('fs')
+const handlebars = require('handlebars')
 const gulp = require('gulp')
+const hb = require('gulp-hb')
 const postcss = require('gulp-postcss')
 
+// Dirty hack to use partials with the .html extension 
+// See https://github.com/shannonmoeller/gulp-hb/issues/41#issuecomment-218832896
+require.extensions['.html'] = (module, filename) => {
+  module.exports = handlebars.compile(fs.readFileSync(filename, 'utf8'))
+}
+
+gulp.task('html', () => (
+  gulp.src('./src/*.html')
+  .pipe(hb({
+    partials: './src/_includes/**/*.{hbs,html}'
+  }))
+  .pipe(gulp.dest('./dist'))
+))
+
 gulp.task('css', () => (
-  gulp.src('web/css/index.css')
+  gulp.src('./src/css/index.css')
   .pipe(gulp.dest('./dist/css'))
 ))
+
+gulp.task('default', ['html', 'css'])
