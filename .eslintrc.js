28ead103c71a14f9004ae866f2ffa50b108c87cd
Damien Erambert <damien.erambert@me.com>
2017-10-13 22:57:09 -0700

    :construction:

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-filename-extension': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+  }
+};
+
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,38 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -43,7 +13,6 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     <div
       style={{
         margin: '0 auto',
@@ -55,10 +24,14 @@ const TemplateWrapper = ({ children }) => (
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+TemplateWrapper.defaultProps = {
+  children: () => null,
+};
+
+export default TemplateWrapper;
