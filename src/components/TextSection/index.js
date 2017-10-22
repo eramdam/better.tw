1d5fabaed57bfa7551d0f49b311b017f57d3631e
Damien Erambert <damien.erambert@me.com>
2017-10-21 19:17:50 -0700

    :wrench: support for render prop in TextSection + for additional props on image tag

--- src/components/TextSection/index.js
+++ src/components/TextSection/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  isFunction,
+} from 'lodash';
 import styles from './index.module.css';
 
 const imgPath = url => `${__PATH_PREFIX__}/img/${url}`;
 
-const TextSection = ({ title, paragraph, image }) => (
-  <section className={styles.textSection}>
-    <div className={styles.textSectionGrid}>
-      <div className={styles.sectionCopy}>
-        <h4>{title}</h4>
-        <p className={styles.sectionParagraph}>{paragraph}</p>
+const renderTextProp = (prop) => {
+  return isFunction(prop) ? prop() : prop;
+};
+
+const TextSection = ({ title, paragraph, image, ...more }) => {
+  return (
+    <section className={styles.textSection}>
+      <div className={styles.textSectionGrid}>
+        <div className={styles.sectionCopy}>
+          <h4>{title}</h4>
+          <p className={styles.sectionParagraph}>{renderTextProp(paragraph)}</p>
+        </div>
+        <img className={styles.sectionImage} src={imgPath(image)} alt="" {...more} />
       </div>
-      <img className={styles.sectionImage} src={imgPath(image)} alt="" />
-    </div>
-  </section>
-);
+    </section>
+  )
+};
 
 
 TextSection.propTypes = {
-  title: PropTypes.string,
-  paragraph: PropTypes.string,
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+  paragraph: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
   image: PropTypes.string,
 };
 
