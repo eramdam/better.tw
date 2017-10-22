0f2b7aa70f37e43461ead4991a67760870f359e7
Damien Erambert <damien.erambert@me.com>
2017-10-21 19:17:25 -0700

    :shirt:

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'react/jsx-filename-extension': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/accessible-emoji': 0,
     'max-len': 0,
     'react/forbid-prop-types': [2, { "forbid": ['any'] }],
   }
