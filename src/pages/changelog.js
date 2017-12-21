4386761e60a63c385ad7064367aa0ab62e4c7ab2
Damien Erambert <damien.erambert@me.com>
2017-12-20 21:28:17 -0800

    :wrench:

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styles from './changelog.module.css';
 
-const ChangelogPage = () => (
-  <div>
-    Hello
-  </div>
-);
+const ChangelogPage = ({ data }) => {
+  const { html } = data.allGithubChangelog.edges[0].node;
+
+  return (
+    <div
+      className={styles.changelog}
+      dangerouslySetInnerHTML={{
+        __html: html,
+      }}
+    />
+  );
+};
 
 export default ChangelogPage;
+
+export const query = graphql`
+  query BTDMarkdownQuery {
+    allGithubChangelog {
+      edges {
+        node {
+          id,
+          html
+        }
+      }
+    }
+  }
+`;
+
