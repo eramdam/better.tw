c498377ae2acfa731cc057bc26b1a84f31a0e20a
Damien Erambert <damien.erambert@me.com>
2017-12-23 21:15:23 -0800

    :wrench: build changelog page using the data grabbed from github

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,20 +1,48 @@
 /* eslint react/no-danger: 0 */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 import styles from './changelog.module.css';
 
 const ChangelogPage = ({ data }) => {
-  const { html } = data.allGithubChangelog.edges[0].node;
+  const releases = data.allGithubRelease.edges.map(e => e.node);
 
   return (
     <div
       className={styles.changelog}
     >
-      <div
-        dangerouslySetInnerHTML={{
-          __html: html,
-        }}
-      />
+      {releases.map((release) => {
+        const formattedDate = format(release.date, 'MM/DD/YYYY');
+
+        return (
+          <div key={release.id} className={styles.singleRelease}>
+            <h1 key={`title-${release.id}`}>
+              {release.title}
+            </h1>
+            <hr />
+            <div
+              key={`content-${release.id}`}
+              className={styles.releaseDescription}
+              dangerouslySetInnerHTML={{
+                __html: release.description,
+              }}
+            />
+            <hr />
+            <footer
+              className={styles.releaseFooter}
+            >
+              <small
+                className={styles.releaseDate}
+              >
+                Released on <time dateTime={release.date}>{formattedDate}</time>
+              </small>
+              <small>
+                <a href={release.url} className={styles.releaseExternalLink}>See on GitHub</a>
+              </small>
+            </footer>
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -27,11 +55,14 @@ export default ChangelogPage;
 
 export const query = graphql`
   query BTDMarkdownQuery {
-    allGithubChangelog {
+    allGithubRelease {
       edges {
         node {
-          id,
-          html
+          id
+          title
+          url
+          description
+          date
         }
       }
     }
