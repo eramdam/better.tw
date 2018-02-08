a186250db9bf0aa888f5b49b30b7cf48efc7f00b
Damien Erambert <damien.erambert@me.com>
2018-02-08 22:17:01 +0100

    :wrench:

--- src/pages/gif.js
+++ src/pages/gif.js
@@ -1,34 +1,9 @@
 
 import React from 'react';
 import gifshot from 'gifshot';
-import FileSaver from 'file-saver';
 import qs from 'query-string';
 
-const dataURItoBlob = (dataURI) => {
-  // convert base64 to raw binary data held in a string
-  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-  const byteString = atob(dataURI.split(',')[1]);
-
-  // separate out the mime component
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to an ArrayBuffer
-  const ab = new ArrayBuffer(byteString.length);
-
-  // create a view into the buffer
-  const ia = new Uint8Array(ab);
-
-  // set the bytes of the buffer to the correct values
-  for (let i = 0; i < byteString.length; i += 1) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  // write the ArrayBuffer to a blob, and you're done
-  const blob = new Blob([ab], { type: mimeString });
-  return blob;
-};
-
-const msgToParent = msg => window.parent.postMessage(msg, 'https://tweetdeck.twitter.com/');
+const msgToParent = msg => window.parent && window.parent.postMessage(msg, 'https://tweetdeck.twitter.com/');
 
 class GifPage extends React.Component {
   componentDidMount() {
@@ -46,7 +21,7 @@ class GifPage extends React.Component {
     const hasAllKeys = neededKeys.every(k => parsed[k]);
 
     // If we don't have all the right keys or we're not in an iframe, we stop everything
-    if (!hasAllKeys || window.top === window.self) {
+    if (!hasAllKeys) {
       return;
     }
 
@@ -59,10 +34,8 @@ class GifPage extends React.Component {
         }
       },
     }, (obj) => {
-      // Report end to BTD
-      msgToParent({ message: 'complete_gif' });
-      const blob = dataURItoBlob(obj.image);
-      FileSaver.saveAs(blob, parsed.name);
+      // Pass final image to BTD
+      msgToParent({ message: 'complete_gif', img: obj.image, name: parsed.name });
     });
   }
 
