a5c3dde8249b25757605fa8a28deee7e2334b309
Damien Erambert <damien@erambert.me>
2021-12-31 20:41:01 -0800

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Custom404: NextPage = () => {
+  return (
+    <div className="w-full py-12 mt-5 my-0 mx-auto prose prose-lg prose-gray prose-a:text-sky-500 prose-a:no-underline hover:prose-a:underline">
+      <h1>4️⃣0️⃣4️⃣ - Not found</h1>
+      <p>The page you&apos;re looking for was not found.</p>
+    </div>
+  );
+};
+
+export default Custom404;
